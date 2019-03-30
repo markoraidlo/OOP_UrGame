8a4d2bc6c2e3bfcb80991483ed0561039f323f12
Marko Raidlo <44472814+markoraidlo@users.noreply.github.com>
2019-03-30 20:59:49 +0200

    Initial commit

---
+++ src/Mängulaud.java
@@ -0,0 +1,3 @@
+public class Mängulaud {
+    
+}
---
+++ src/UurMäng.java
@@ -0,0 +1,7 @@
+public class UurMäng {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        //Siia tuleb main mängu loop.
+    }
+}
