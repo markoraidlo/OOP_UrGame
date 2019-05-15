2626dd4e14a17c4b53b9b79885c96bb43a33fc4e
markoraidlo <marko.raidlo@gmail.com>
2019-05-16 00:32:32 +0300

    Revert "Merge branch 'master' into poolik-mängulaud"
    This reverts commit e87d50acd48aa1bf17ae16a86ffa135217f43ee5, reversing
    changes made to b2fb1ee37116f6feafd7c1111504b3ec36bb1b05.

--- Graafika.java
+++ Graafika.java
@@ -29,6 +29,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -184,24 +185,15 @@ public class Graafika extends Application {
         new AnimationTimer() {
             @Override public void handle(long currentNanoTime) {
                 //Objektide loomine.
-                List<Mängunupp> lubatud;
+                List<Mängunupp> lubatud = new ArrayList<>();
                 int silmadeArv;
                 int liigutatudNuppuAsukoht;
-
-                try {
-                    Graafika.fileWriter(mängulaud.tagastaLaud(), failiNimi);
-                }
-                catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-
 
 
                 if (mängulaud.võiduKontroll() != 0)
                     this.stop();
 
                 //Äkki saab niimoodi erinevat teksti väljastada?
-
 
               
   
@@ -250,22 +242,9 @@ public class Graafika extends Application {
                         //TODO: Jääd vahele animation / Veeretasid nulli
                         tekstField.setText("Jääd vahele!");
                         //Jääd vahele
-
                     }
 
-                    //Kontroll(Mis nuppudega võib käia)
-                    lubatud = mängulaud.kontroll(silmadeArv, mängulaud.getMängijaAlgus(), mängulaud.getMängijaTee(), mängulaud.getArvutiTee());
-                    //TODO: Lubatud nuppud
-                    if (lubatud.size() == 0) {
-                        tekst.setText("Jääd vahele!");
-                        //TODO: Jääd vahele / Pole võimalik käia
-                        break;
-                    }
 
-                    //TODO: Nuppu valik
-
-                    // Toimub nuppu liigutamine kui seda saab liigutada, kui ei saa siis tuleb uuesti sisestada.
-                    mängulaud.liiguta(mängulaud.getMängijaNuppud().get(sisend - 1), silmadeArv);
 
 
                     //Boonus ruudu kontroll
@@ -281,30 +260,20 @@ public class Graafika extends Application {
 */
 
 
-                    //Logi
-                    try {
-                        Graafika.fileWriter(mängulaud.tagastaLaud(), failiNimi);
-                    }
-                    catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
+                    //ARVUTI LOOP:
 
-                    //Boonus ruudu kontroll
-                    liigutatudNuppuAsukoht = mängulaud.getMängijaTee().indexOf(mängulaud.getMängijaNuppud().get(sisend - 1));
-                    if (liigutatudNuppuAsukoht == 3 || liigutatudNuppuAsukoht == 7 ||liigutatudNuppuAsukoht == 13) {
-                        //TODO: Boonusruudu animatsioon
-                        tekst.setText("Astusid boonusruudule!");
-                        continue;
+                    //Un disable loop
+/*
+                    while (true){
+
                     }
-                    else
-                        break;
-
-                }
-
+                    */
+                    veeretaNupp.setDisable(false);
+                });
 
 
                 //ARVUTI LOOP:
-
+                /*
                 //TODO: "Vastase käik" animation
                 while (true) {
                     silmadeArv = täring.veereta();
